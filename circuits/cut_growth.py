The modified Karger function for the minimum cut calculation, which was giving inconsistent values, was fixed. Functions for better graph visualization were also added, including the ability to graph based on excluded nodes. Everything is working correctly now, only the exact entropy calculations remain to be done

circuits/cut_growth.py
@@ -3,62 +3,109 @@ from functions import min_cut
 import multiprocessing
 import numpy as np
 import os
-from functools import partial
 import concurrent.futures
+from numba import jit
 
-# Función que se encargará de calcular el min_cut para un único trial
-def calculate_min_cut(n_qubits, depth, trial, exclude_nodes, dat_file_path_circuits):
-    name = f"{trial}"
+# Function that will be responsible for calculating the min_cut for a single trial
+
+def calculate_min_cut(n_qubits, depth, circuits_trial, exclude_nodes, cut_trials, dat_file_path_circuits):
+    name = f"{circuits_trial}"
     # Generar circuito aleatorio
-    qc = random_circuit.ramdon_circuit(n_qubits=n_qubits, depth=depth, name=name, bol_dat=False, dat_file_path=dat_file_path_circuits)
+    qc = random_circuit.random_circuit(n_qubits=n_qubits, depth=depth, name=name, bol_dat=False, dat_file_path=dat_file_path_circuits)
     # Calcular min_cut
-    min_cut_value = min_cut.min_cut(qc, exclude_nodes, 10, False, False)
+    min_cut_value = min_cut.min_cut(qc, exclude_nodes, cut_trials, False, False)
     return min_cut_value
 
-# Función que se encargará de manejar todos los trials para una combinación específica de n_qubits y depth
-def process_trials(n_qubits, depth, exclude_nodes, trials, dat_file_path_circuits):
+# Function that will be responsible for handling all trials for a specific combination of n_qubits and depth
+
+def process_trials(n_qubits, depth, exclude_nodes, circuits_trials, cut_trials, dat_file_path_circuits):
     min_cut_values = []
     
-    # Usar un executor para manejar los trials en paralelo
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        # Preparar una lista de tareas para los trials
-        futures = [executor.submit(calculate_min_cut, n_qubits, depth, trial, exclude_nodes, dat_file_path_circuits) for trial in range(trials)]
+    # Use an executor to handle trials in parallel
+    with concurrent.futures.ProcessPoolExecutor(max_workers=24) as executor:
+        # Prepare a task list for trials
+        futures = [executor.submit(calculate_min_cut, n_qubits, depth, trial, exclude_nodes, cut_trials, dat_file_path_circuits) for trial in range(circuits_trials)]
         
-        # Recoger los resultados a medida que se completan
+        # Collect results as they are completed
         for future in concurrent.futures.as_completed(futures):
             min_cut_values.append(future.result())
     
-    # Calcular el valor promedio de min_cut
+    # Calculate the average value of min_cut
     return np.mean(min_cut_values)
 
-# Función principal
-def cut_growth(total_qubits, total_depth, exclude_nodes, trials):
+# Main function
+
+def cut_growth(total_qubits, total_depth, exclude_nodes, circuits_trials, cut_trials, fixed=None):
     
     dat_file_path_circuits = f'results/circuits/qubits{total_qubits}_depth{total_depth}/circuits.dat'
-    dat_file_path_results = f'results/circuits/qubits{total_qubits}_depth{total_depth}/results.dat'
+    dat_file_path_results = f'results/circuits/qubits{total_qubits}_depth{total_depth}/results_{exclude_nodes}.dat'
+    
+    if fixed is not None and fixed == 'qubits':
+        initial_qubit = total_qubits
+    else:  
+        initial_qubit = 3
+        
+    if fixed is not None and fixed == 'depth':
+        initial_depth = total_depth
+    else:  
+        initial_depth = 1
     
     if not os.path.exists(f'results/circuits/qubits{total_qubits}_depth{total_depth}'):
         os.makedirs(f'results/circuits/qubits{total_qubits}_depth{total_depth}')
     
-    # Abrir archivo de resultados
+    # Open results file
     with open(dat_file_path_results, 'w') as dat_file:
         dat_file.write(f'qubits depth min_cut_value\n')
         
-        # Iterar sobre cada combinación de n_qubits
-        with concurrent.futures.ProcessPoolExecutor() as executor:
-            for n_qubits in range(total_qubits, total_qubits + 1):
-                # Preparar una lista de tareas para cada profundidad
-                futures = {depth: executor.submit(process_trials, n_qubits, depth, exclude_nodes, trials, dat_file_path_circuits) for depth in range(1, total_depth + 1)}
-                
-                # Recoger los resultados y escribirlos en el archivo
-                for depth, future in futures.items():
-                    min_cut_value = future.result()
-                    dat_file.write(f'{n_qubits} {depth} {min_cut_value}\n')
+        # Iterate over each combination of n_qubits
+        with concurrent.futures.ProcessPoolExecutor(max_workers=24) as executor:
+                for n_qubits in range(initial_qubit, total_qubits + 1):
+                    # Prepare a task list for each depth
+                    futures = {depth: executor.submit(process_trials, n_qubits, depth, exclude_nodes, circuits_trials, cut_trials, dat_file_path_circuits) for depth in range(initial_depth, total_depth + 1)}
+                    
+                    # Collect the results and write them to the file
+                    for depth, future in futures.items():
+                        min_cut_value = future.result()
+                        dat_file.write(f'{n_qubits} {depth} {min_cut_value}\n')
+
+
+def cut_growth_exclude(n_qubits, depth, circuits_trials, cut_trials):
+    
+    dat_file_path_circuits = f'results/circuits/qubits{n_qubits}_depth{depth}/circuits.dat'
+    dat_file_path_results = f'results/circuits/qubits{n_qubits}_depth{depth}/results_excludes.dat'
+
+    if not os.path.exists(f'results/circuits/qubits{n_qubits}_depth{depth}'):
+        os.makedirs(f'results/circuits/qubits{n_qubits}_depth{depth}')
+
+    with open(dat_file_path_results, 'w') as dat_file:
+        dat_file.write(f'exclude_nodes min_cut_value\n')
+
+        with concurrent.futures.ProcessPoolExecutor(max_workers=24) as executor:
+
+            futures = {
+                exclude_node: executor.submit(
+                    process_trials,
+                    n_qubits,
+                    depth,
+                    [exclude_node] if exclude_node == n_qubits - 1 else [exclude_node, exclude_node + 1],  
+                    circuits_trials,
+                    cut_trials,
+                    dat_file_path_circuits
+                ) for exclude_node in range(0, n_qubits-1)
+            }
+            
+
+            for exclude_node, future in futures.items():
+                min_cut_value = future.result()
+
+                dat_file.write(f'{exclude_node} {min_cut_value}\n')
 
 
 
 
-# Sin paralelizar
+
+
+# Unparallelized
 # def cut_growth(total_qubits, total_depth, exclude_nodes, trials):
     
 #     dat_file_path_circuits = f'results/circuits/qubits{total_qubits}_depth{total_depth}/circuits.dat'
@@ -69,18 +116,16 @@ def cut_growth(total_qubits, total_depth, exclude_nodes, trials):
     
 #     with open(dat_file_path_results, 'w') as dat_file:
 #         dat_file.write(f'qubits depth min_cut_value\n')
-#         with multiprocessing.Pool(processes=24) as pool:
-#             for n_qubits in range(3, total_qubits + 1):
-#                 for depth in range(1, total_depth + 1):
-#                     min_cut_values = []
-#                     for trial in range(trials):
-#                         name = f"{trial}"
-#                         qc = random_circuit.ramdon_circuit(n_qubits=n_qubits, depth=depth, name=name, bol_dat=False, dat_file_path=dat_file_path_circuits)
-#                         min_cut_value = min_cut.min_cut(qc, exclude_nodes, trials, False, False)
-#                         min_cut_values.append(min_cut_value)
+#         for n_qubits in range(total_qubits, total_qubits + 1):
+#             for depth in range(1, total_depth + 1):
+#                 min_cut_values = []
+#                 for trial in range(trials):
+#                     name = f"{trial}"
+#                     qc = random_circuit.random_circuit(n_qubits=n_qubits, depth=depth, name=name, bol_dat=False, dat_file_path=dat_file_path_circuits)
+#                     min_cut_value = min_cut.min_cut(qc, exclude_nodes, 10, False, False)
+#                     min_cut_values.append(min_cut_value)
 
-#                     min_cut_value = np.mean(min_cut_values)
-#                     # save data
-#                     dat_file.write(f'{n_qubits} {depth} {min_cut_value}\n')
-                    
-                    
+#                 min_cut_value = np.mean(min_cut_values)
+#                 # save data
+#                 dat_file.write(f'{n_qubits} {depth} {min_cut_value}\n')
+